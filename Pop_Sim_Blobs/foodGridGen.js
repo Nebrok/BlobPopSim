c2565b7862c6a909023d7bd5135dd577c71d0675
Nebrok <nebrok.red@outlook.com>
2022-07-14 21:47:55 +1000

    added FoodGrid Class

---
+++ Pop_Sim_Blobs/foodGridGen.js
@@ -0,0 +1,14 @@
+
+class FoodGrid {
+    constructor(worldHeight, worldWidth) {
+        let _height = worldHeight;
+        let _width = worldWidth;
+
+        console.log("Food Grid Created");
+    }
+
+    addToGrid(grid) {
+        console.log("Add to grid function called");
+    }
+
+}
