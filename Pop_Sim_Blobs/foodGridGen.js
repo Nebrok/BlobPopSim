83ef6461d3909694463367542c6999c87b0c4b9e
Nebrok <nebrok.red@outlook.com>
2022-07-15 23:29:31 +1000

    Init function of PoissonGen class created

--- Pop_Sim_Blobs/foodGridGen.js
+++ Pop_Sim_Blobs/foodGridGen.js
@@ -1,14 +1,36 @@
 
-class FoodGrid {
+class PoissonFoodGrid {
     constructor(worldHeight, worldWidth) {
         let _height = worldHeight;
         let _width = worldWidth;
 
+        let _r;
+        let _k;
+        let _w;
+        let _cols;
+        let _rows;
+        let grid = [];
+        let active = [];
+
+
         console.log("Food Grid Created");
     }
 
-    addToGrid(grid) {
-        console.log("Add to grid function called");
+    initPoissonAlgo(r, k) {
+        _r = r;
+        _k = k;
+        _w = _r/Math.sqrt(2);
+
+        _cols = floor(width/w)
+        _rows = floor(height/w)
+
     }
 
+    generateGrid(grid) {
+        console.log("Grid Generated")
+    }
+
+    _distance(pos1, pos2) {
+        return Math.sqrt((pos1.x - pos2.x)**2 + (pos1.y - pos2.y)**2)
+      }
 }
